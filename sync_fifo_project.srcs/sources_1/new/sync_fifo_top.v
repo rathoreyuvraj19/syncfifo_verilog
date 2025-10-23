362e38f3b8c627a38e60d16f503ca1003cef6903
Yuvraj <rathore.yuvraj19@gmail.com>
2025-10-23 14:05:09 +0530

    Add top module

---
+++ sync_fifo_project.srcs/sources_1/new/sync_fifo_top.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.10.2025 14:04:37
+// Design Name: 
+// Module Name: sync_fifo_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sync_fifo_top(
+    input i_clk,
+    input i_reset,
+    output o_output
+    );
+endmodule
